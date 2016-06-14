f90400f5ac6425286b84e50dd717a79d1b86cea0
Ammar Shah <ammarshah123@gmail.com>
2016-06-15 02:43:41 +0500

    Simulating the web-api, get data from fake api server

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
+import 'rxjs/add/operator/toPromise';
+
 
 @Injectable()
 
 export class HeroService {
-  getHeroes() {
-    return Promise.resolve(HEROES);
+	private heroesUrl = 'app/heroes';  // URL to web api
+	
+	constructor(private http: Http) { }
+
+	getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+               .toPromise()
+               .then(response => response.json().data)
+               .catch(this.handleError);
   }
 
   // See the "Take it slow" appendix
@@ -19,6 +29,11 @@ export class HeroService {
   getHero(id: number) {
     return this.getHeroes()
                 .then(heroes => heroes.filter(hero => hero.id === id)[0]);
+	}
+
+	private handleError(error: any) {
+		console.error('An error occurred', error);
+		return Promise.reject(error.message || error);
 	}
 
 }
