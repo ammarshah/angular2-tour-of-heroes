a330d6b5d00c6fc4c612ec7e903130787f8f4392
Ammar Shah <ammarshah123@gmail.com>
2016-06-14 23:26:08 +0500

    Import router in AppComponent and add base tag in html file, which is necessary for routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component }       from '@angular/core';
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
 @Component({
   selector: 'my-app',
@@ -8,8 +9,8 @@ import { HeroesComponent } from './heroes.component';
     <h1>{{title}}</h1>
     <my-heroes></my-heroes>
   `,
-  directives: [HeroesComponent],
-  providers: [HeroService]
+  directives: [ROUTER_DIRECTIVES],
+  providers: [HeroService, ROUTER_PROVIDERS]
 })
 export class AppComponent {
   title = 'Tour of Heroes';
