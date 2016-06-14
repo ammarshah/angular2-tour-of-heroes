38c45035708f0c143d967542424468e6f8b793d3
Ammar Shah <ammarshah123@gmail.com>
2016-06-14 22:24:42 +0500

    Act on a promise

--- app/app.component.ts
+++ app/app.component.ts
@@ -84,7 +84,7 @@ export class AppComponent implements OnInit {
     constructor(private heroService: HeroService) { }
 
     getHeroes() {
-        this.heroes = this.heroService.getHeroes();
+        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
     }
 
     ngOnInit() {
